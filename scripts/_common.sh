7d197c895911503b376b9dbacb451dc41493736e
oiseauroch <contact@oiseauroch.fr>
2022-07-27 10:00:43 +0200

    initial commit

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,12 +4,42 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
 
 #=================================================
 # PERSONAL HELPERS
 #=================================================
+
+
+GARAGE_VERSION="0.7.2.1"
+
+# inspired by restic helper
+install_garage () {
+  architecture=$(uname -m)
+  arch=''
+  case $architecture in
+    i386|i686)
+      arch="i686"
+      ;;
+    x86_64)
+      arch=x86_64
+      ;;
+    armv*)
+      arch=armv6l
+      ;;
+    aarch64)
+      arch=aarch64
+      ;;
+    *)
+      echo
+      ynh_die --message="Unsupported architecture \"$architecture\""
+      ;;
+  esac
+  wget https://garagehq.deuxfleurs.fr/_releases/v$GARAGE_VERSION/$arch-unknown-linux-musl/garage -O "$app" 2>&1 >/dev/null
+    chmod +x "$app"
+  else
+    ynh_die --message="\nDownloaded file does not match expected sha256 sum, aborting"
+  fi
+}
 
 #=================================================
 # EXPERIMENTAL HELPERS
