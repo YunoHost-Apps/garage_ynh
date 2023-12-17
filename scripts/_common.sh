2f7a656d4fdd68b40d11e5cf908af71575c65dc5
oiseauroch <contact@oiseauroch.fr>
2023-12-17 09:22:57 +0100

    fix layout show

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -43,7 +43,7 @@ apply_layout() {
     
 	garage_command=$1
 	$garage_command layout show 2>/dev/null
-	if [ "$($garage_command -c garage.toml layout show | grep 'This new layout cannot yet be applied')" ]
+	if [ "$($garage_command layout show | grep 'This new layout cannot yet be applied')" ]
 	then
         ynh_print_warn --message="Unable to apply layout. No enough nodes"
 		return 0
