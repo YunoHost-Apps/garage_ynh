483f9ece1e857900aed53c783d1ff696e729184c
gredin67 <gredin67@sans-nuage.fr>
2026-02-01 13:27:14 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source /usr/share/yunohost/helpers
-
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
